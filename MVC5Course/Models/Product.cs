c9cc005b6c5ed0a3f8ad9cc6de62f7e1d441f0b9
楚鈞 <@CJWANG>
2017-05-06 13:22:29 +0800

    08 練習在 Product 資料模型加入 [Required] 驗證屬性

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,7 +11,8 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,9 +22,13 @@ namespace MVC5Course.Models
         }
     
         public int ProductId { get; set; }
+        [Required(ErrorMessage ="請輸入商品名稱")]
         public string ProductName { get; set; }
+        [Required(ErrorMessage = "請輸入Price")]
         public Nullable<decimal> Price { get; set; }
+        [Required(ErrorMessage = "請選擇Active")]
         public Nullable<bool> Active { get; set; }
+        [Required(ErrorMessage = "請輸入Stock")]
         public Nullable<decimal> Stock { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
