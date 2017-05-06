1e30e22ca0c09b10230c9a6586ccf1cea2223340
楚鈞 <@CJWANG>
2017-05-06 16:24:49 +0800

    13 練習在 ProductName 加上 [DisplayName] 自訂欄位標題名稱

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -2,8 +2,9 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ProductMetaData))]
     public partial class Product
     {
@@ -17,14 +18,18 @@ namespace MVC5Course.Models
         [Required(ErrorMessage = "請輸入商品名稱")]
         [MinLength(3), MaxLength(30)]
         [RegularExpression(".+-.+", ErrorMessage = "商品名稱格式錯誤")]
+        [DisplayName("商品名稱")]
         public string ProductName { get; set; }
         [Required(ErrorMessage = "請輸入Price")]
         [Range(0, 9999999, ErrorMessage = "請輸入正確的數字")]
+        [DisplayName("商品價格")]
         [DisplayFormat(ApplyFormatInEditMode =true,DataFormatString ="{0:0}")]
         public Nullable<decimal> Price { get; set; }
+        [DisplayName("是否上架")]
         [Required(ErrorMessage = "請選擇Active")]
         public Nullable<bool> Active { get; set; }
         [Required(ErrorMessage = "請輸入Stock")]
+        [DisplayName("商品庫存")]
         [Range(0, 100, ErrorMessage = "請輸入0-100")]
         public Nullable<decimal> Stock { get; set; }
 
