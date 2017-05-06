dce38b6987f562608337b6d5aa9b4b49d0a7df6a
楚鈞 <@CJWANG>
2017-05-06 13:29:17 +0800

    09 練習在 Product 資料模型加入更多的驗證屬性

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -23,12 +23,16 @@ namespace MVC5Course.Models
     
         public int ProductId { get; set; }
         [Required(ErrorMessage ="請輸入商品名稱")]
+        [MinLength(3),MaxLength(30)]
+        [RegularExpression(".+-.+",ErrorMessage ="商品名稱格式錯誤")]
         public string ProductName { get; set; }
         [Required(ErrorMessage = "請輸入Price")]
+        [Range(0,9999999,ErrorMessage ="請輸入正確的數字")]
         public Nullable<decimal> Price { get; set; }
         [Required(ErrorMessage = "請選擇Active")]
         public Nullable<bool> Active { get; set; }
         [Required(ErrorMessage = "請輸入Stock")]
+        [Range(0,100, ErrorMessage = "請輸入0-100")]
         public Nullable<decimal> Stock { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
