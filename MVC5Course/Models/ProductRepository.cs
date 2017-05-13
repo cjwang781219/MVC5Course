db6ffcb6e47a282cd2ff4827151c3d879c7cf956
楚鈞 <@CJWANG>
2017-05-13 10:49:02 +0800

    28.參考保哥的 code 調整edit的程式

--- MVC5Course/Models/ProductRepository.cs
+++ MVC5Course/Models/ProductRepository.cs
@@ -2,27 +2,48 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using MVC5Course.Models.ViewModels;
+using System.Data.Entity;
 
 namespace MVC5Course.Models
 {   
 	public  class ProductRepository : EFRepository<Product>, IProductRepository
 	{
+        public override IQueryable<Product> All()
+        {
+            return base.All().Where(p => !p.Is刪除);
+        }
+
+        public IQueryable<Product> All(bool showAll)
+        {
+            if (showAll)
+            {
+                return base.All();
+            }
+            else
+            {
+                return this.All();
+            }
+        }
+
         public Product getProductByID(int ID)
         {
-            return this.All().FirstOrDefault(p => p.ProductId == ID);
+            return this.All(true).FirstOrDefault(p => p.ProductId == ID);
         }
 
         public void Edit(Product product)
         {
-            var data = getProductByID(product.ProductId);
+            //我的寫法 
+            //var data = getProductByID(product.ProductId);
+            //this.Delete(data);
+            //this.Add(product);
 
-            this.Delete(data);
-            this.Add(product);
+            //保哥的
+            this.UnitOfWork.Context.Entry(product).State = EntityState.Modified;
         }
 
         public IQueryable<ProdectLiteVM> getProductList()
         {
-            var data = this.All().Where(p => p.Active == true).OrderByDescending(p => p.ProductId)
+            var data = this.All(true).Where(p => p.Active == true).OrderByDescending(p => p.ProductId)
                 .Select(x => new ProdectLiteVM()
                 {
                     ProductId = x.ProductId,
